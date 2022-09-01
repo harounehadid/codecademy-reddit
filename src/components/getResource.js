a0ff8ae34b338487354b7f68a73e35616910aa85
Haroune Hdd <75623924+HarouneHdd@users.noreply.github.com>
2022-09-01 12:27:25 +0100

    Add more resources

--- src/components/getResource.js
+++ src/components/getResource.js
@@ -2,3 +2,8 @@
 export const Logo = {
     reddit: require('../resources/reddit-logo.jpg')
 };
+
+export const Icons = {
+    home: require('../resources/home-icon.svg').default,
+    dropDown: require('../resources/drop-down.svg').default
+};
