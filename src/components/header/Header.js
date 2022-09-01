97538bcd085a374bef4797b7298a218c8d7a2f56
Haroune Hdd <75623924+HarouneHdd@users.noreply.github.com>
2022-09-01 12:27:06 +0100

    Work on header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,24 +1,28 @@
 import Searchbar from '../../features/searchbar/Searchbar';
-import { Logo } from '../getResource';
+import { Logo, Icons } from '../getResource';
 import './Header.css';
 
 const Header = () => {
     return (
-        <div className='Header center
-                        fill-container-width
+        <div className='Header
+                        center
+                        fill-screen-width
                         flex-diplay
                         flex-vertical-center'>
-            <a className='Header-logo' href='reddit.com'>
-                <img
-                    src={Logo.reddit} 
-                    alt='Reddit Logo' 
-                    className='fill-container-height'    
-                />
+
+            <a href='reddit.com'>
+                <img src={Logo.reddit} alt='Reddit Logo' className='Header-logo' />
             </a>
-            <button className='Header-home-btn
-                               fill-container-height'>
-                
+
+            <button alt='Home Button Drop Down'
+                    className='Header-home-btn
+                               flex-diplay
+                               flex-vertical-center'>
+                <img src={Icons.home} alt='' className='Header-home-btn-home-icon' />
+                <p>Home</p>
+                <img src={Icons.dropDown} alt='' className='Header-home-btn-dropdown-icon' />
             </button>
+            
             {/*<Searchbar />*/}
         </div>
     );
