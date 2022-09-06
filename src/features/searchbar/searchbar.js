cba352a1ad6bc3f09da29b4385bce210f9b3d360
Haroune Hdd <75623924+HarouneHdd@users.noreply.github.com>
2022-09-06 12:34:04 +0100

    Complete working on searchbar style

--- src/features/searchbar/searchbar.js
+++ src/features/searchbar/searchbar.js
@@ -3,12 +3,20 @@ import { Icons } from '../../components/getResource';
 
 const Searchbar = () => {
     return (
-        <div className='Searchbar'>
+        <div className='Searchbar
+                        flex-diplay
+                        flex-vertical-center'>
+
             <img src={Icons.searchbar} 
                  alt=''
                  className='Searchbar-icon' />
 
-            <input type='text' />
+            <input type='text' 
+                   name='searchbar' 
+                   id='searchbar' 
+                   placeholder='Search Reddit'
+                   className='Searchbar-input'
+                   />
             
         </div>
     );
