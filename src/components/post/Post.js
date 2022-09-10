943806b2d4cc82749222a53ba2836065901f78ea
Haroune Hdd <75623924+HarouneHdd@users.noreply.github.com>
2022-09-10 12:33:24 +0100

    Add post folder

---
+++ src/components/post/Post.js
@@ -0,0 +1,18 @@
+import './Post.css';
+
+const Post = () => {
+    return (
+        <>
+            <div className='Post'>
+                <div className='Post-vote-section'>
+
+                </div>
+                <div className='Post-display-info'>
+
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Post;
