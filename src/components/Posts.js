68e38ac42145e6bb449e0a146256c51eae7198ad
Haroune Hdd <75623924+HarouneHdd@users.noreply.github.com>
2022-09-12 06:49:08 +0100

    Add hard code to posts component

---
+++ src/components/Posts.js
@@ -0,0 +1,15 @@
+import Post from "./post/Post";
+
+const Posts = () => {
+    return (
+        <div className='flex-diplay
+                        flex-horizontal-center
+                        flex-column-direction
+                        margin-center'>
+            <Post />
+            <Post />
+        </div>
+    );
+}
+
+export default Posts;
