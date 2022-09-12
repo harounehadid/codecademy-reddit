568e8bfb03a8c9b88b9fb96f82bc4f8b6c0c8a83
Haroune Hdd <75623924+HarouneHdd@users.noreply.github.com>
2022-09-12 06:09:15 +0100

    Add more resources

--- src/components/getResource.js
+++ src/components/getResource.js
@@ -6,5 +6,9 @@ export const Logo = {
 export const Icons = {
     home: require('../resources/home-icon.svg').default,
     dropDown: require('../resources/drop-down.svg').default,
-    searchbar: require('../resources/search-icon.svg').default
+    searchbar: require('../resources/search-icon.svg').default,
+    voteUp: require('../resources/vote-up-icon.svg').default,
+    voteDown: require('../resources/vote-down-icon.svg').default,
+    comment: require('../resources/comment-icon.svg').default,
+    share: require('../resources/share-icon.svg').default
 };
