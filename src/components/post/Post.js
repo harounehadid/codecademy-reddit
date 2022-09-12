9d77bde5c7d249821d0def98fe21dbffe31d455c
Haroune Hdd <75623924+HarouneHdd@users.noreply.github.com>
2022-09-12 06:51:14 +0100

    Add voting section

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,15 +1,24 @@
 import './Post.css';
+import { Icons } from '../getResource';
 
 const Post = () => {
     return (
         <>
             <div className='Post'>
-                <div className='Post-vote-section'>
 
+                <div className='Post-vote-section
+                                flex-diplay
+                                flex-vertical-center
+                                flex-column-direction'>
+                    <img src={Icons.voteUp} alt='Vote up' />
+                    <p>300</p>
+                    <img src={Icons.voteDown} alt='Vote down' />
                 </div>
+
                 <div className='Post-display-info'>
 
                 </div>
+
             </div>
         </>
     );
