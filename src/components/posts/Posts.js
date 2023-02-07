1ea8d30e270e9dac74cd6cd78f00fd8b5234a4b5
Haroune Hdd <75623924+HarouneHdd@users.noreply.github.com>
2023-02-07 11:09:28 +0100

    Clean the code more. Use async...await to fetch data

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -3,40 +3,53 @@ import Post from "../post/Post";
 import { useState, useEffect } from "react";
 
 const Posts = () => {
-    let url = `https://www.reddit.com/r/pics.json`;
+    let endpoint = `https://www.reddit.com/r/pics.json`;
 
     // Hold data about reddit posts
     let [postsData, setPostsData] = useState([]);
+
     // Fetch posts from the link
-    useEffect(() => {
-        fetch(url)
-        .then(response => response.json())
-        .then(body => {
-            // Get only posts with images
-            const filteredPosts = body.data.children.filter(fetchedPost => fetchedPost.data.post_hint === 'image');
+    const fetchData = () => {
+        return async () => {
+            try {
+                const response = await fetch(endpoint);
 
-            const newPosts = filteredPosts.map(fetchedPost => {
-                let data = {};
-
-                // console.log(fetchedPost.data);
-
-                data.id = fetchedPost.data.id;
-                data.postURL = 'https://www.reddit.com' + fetchedPost.data.permalink;
-                data.media = fetchedPost.data.url_overridden_by_dest;
-                data.authorName = fetchedPost.data.author;
-                data.time = fetchedPost.data.created_utc;
-                data.title = fetchedPost.data.title;
-                data.ups = fetchedPost.data.ups;
-                data.commentsNum = fetchedPost.data.num_comments;
-                
-                return data;
-            });
-
-            setPostsData(newPosts);
-        })
-        .catch(error => console.log(error));
+                if (response.ok) {
+                    const body = await response.json();
+                    
+                    // Get only posts with images
+                    const filteredPosts = body.data.children.filter(fetchedPost => fetchedPost.data.post_hint === 'image');
         
-    }, []);
+                    const newPosts = filteredPosts.map(fetchedPost => {
+                        let data = {};
+        
+                        console.log(fetchedPost.data);
+        
+                        data.id = fetchedPost.data.id;
+                        data.postURL = 'https://www.reddit.com' + fetchedPost.data.permalink;
+                        data.media = fetchedPost.data.url_overridden_by_dest;
+                        data.authorName = fetchedPost.data.author;
+                        data.time = fetchedPost.data.created_utc;
+                        data.title = fetchedPost.data.title;
+                        data.ups = fetchedPost.data.ups;
+                        data.commentsNum = fetchedPost.data.num_comments;
+                        
+                        return data;
+                    });
+        
+                    setPostsData(newPosts);
+                }
+                else {
+                    throw new Error('Request Failed!');
+                }
+            }
+            catch(error) {
+                console.log(error);
+            }
+        }
+    }
+    // connecting fetch thunk to useEffect
+    useEffect(fetchData, [endpoint]);
 
     return (
         <div className={`flex-diplay
