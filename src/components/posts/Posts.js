465b92a5732b686749ac12b9baabf5ed9d82e01f
Haroune Hdd <75623924+HarouneHdd@users.noreply.github.com>
2023-02-09 17:42:55 +0100

    Fix bug with how the posts component behaved cause by its style. Use fully local style instead of global styles

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -4,25 +4,28 @@ import { useSelector, useDispatch } from 'react-redux';
 import { fetchPosts, selectPosts } from './postsSlice';
 import PostContainer from '../post/PostContainer';
 import { selectSearchTerm } from '../../features/searchbar/searchbarSlice';
+import { getSubredditLink } from '../getResource';
 
 const Posts = () => {
     const dispatch = useDispatch();
 
-    let endpoint = `https://www.reddit.com/r/pics.json`;
+    let endpoint;
+
+    useEffect(() => {
+        const randomIndex = Math.floor(Math.random() * 3);
+        endpoint = getSubredditLink(randomIndex);
+    }, []);
 
     const fechtedPosts = useSelector(selectPosts);
+    
     useEffect(() => {
         dispatch(fetchPosts(endpoint));
-    }, [endpoint]);
+    }, [endpoint, dispatch]);
 
     const searchTerm = useSelector(selectSearchTerm);
 
     return (
-        <div className={`flex-diplay
-                         flex-horizontal-center
-                         flex-column-direction
-                         margin-center
-                         ${styles['posts']}`}>
+        <div className={styles['container']}>
 
             {
                 fechtedPosts
